de3e62a0d0b72652b79505fa106cb7b2e27bdf68
nakavandi-cell <nakavandi@gmail.com>
2026-08-15 18:29:08 +0330

    Refactor provider declarations for clarity

--- lib/core/providers/providers.dart
+++ lib/core/providers/providers.dart
@@ -1,4 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../models/checklist_model.dart';
+import '../models/equipment_model.dart';
+import '../models/inspection_model.dart';
 import '../repositories/checklist_repository.dart';
 import '../repositories/equipment_repository.dart';
 import '../repositories/inspection_repository.dart';
@@ -15,25 +19,19 @@ final inspectionRepositoryProvider = Provider<InspectionRepository>((ref) {
   return InspectionRepository();
 });
 
-// لیست چک‌لیست‌ها برای منوی اصلی
 final allChecklistsProvider = FutureProvider<List<ChecklistModel>>((ref) {
   return ref.watch(checklistRepositoryProvider).getAllChecklists();
 });
 
-// چک‌لیست‌های یک دسته (مثلاً برق، حریق، اماکن)
 final checklistsByCategoryProvider =
     FutureProvider.family<List<ChecklistModel>, String>((ref, category) {
   return ref.watch(checklistRepositoryProvider).getByCategory(category);
 });
 
-// لیست تجهیزات
-final allEquipmentsProvider =
-    FutureProvider<List<EquipmentModel>>((ref) {
+final allEquipmentsProvider = FutureProvider<List<EquipmentModel>>((ref) {
   return ref.watch(equipmentRepositoryProvider).getAll();
 });
 
-// پیش‌نویس‌های بازرسی برای ادامه
-final draftInspectionsProvider =
-    FutureProvider<List<InspectionModel>>((ref) {
+final draftInspectionsProvider = FutureProvider<List<InspectionModel>>((ref) {
   return ref.watch(inspectionRepositoryProvider).getDraftInspections();
 });
