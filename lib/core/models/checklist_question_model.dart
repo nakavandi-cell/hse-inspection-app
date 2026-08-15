8b532c9e00e3b4f10c70836b091bf8eee07ec8b3
nakavandi-cell <nakavandi@gmail.com>
2026-08-15 21:45:19 +0330

    Refactor ChecklistQuestionModel for JSON compatibility
    Updated the ChecklistQuestionModel to use snake_case for JSON mapping and added a fromMap factory constructor.

--- lib/core/models/checklist_question_model.dart
+++ lib/core/models/checklist_question_model.dart
@@ -2,10 +2,10 @@ class ChecklistQuestionModel {
   final String id;
   final String checklistId;
   final String text;
-  final String type; // yesno / text / number / select
+  final String type;
   final bool requiredField;
 
-  ChecklistQuestionModel({
+  const ChecklistQuestionModel({
     required this.id,
     required this.checklistId,
     required this.text,
@@ -13,21 +13,29 @@ class ChecklistQuestionModel {
     required this.requiredField,
   });
 
-  Map<String, dynamic> toJson() => {
+  factory ChecklistQuestionModel.fromJson(Map<String, dynamic> json) =>
+      ChecklistQuestionModel(
+        id: json['id'] as String,
+        checklistId: json['checklistId'] as String,
+        text: json['text'] as String,
+        type: json['type'] as String,
+        requiredField: json['requiredField'] as bool? ?? true,
+      );
+
+  Map<String, dynamic> toMap() => {
         'id': id,
-        'checklistId': checklistId,
+        'checklist_id': checklistId,
         'text': text,
         'type': type,
-        'requiredField': requiredField,
+        'required_field': requiredField ? 1 : 0,
       };
 
-  factory ChecklistQuestionModel.fromJson(Map<String, dynamic> json) {
-    return ChecklistQuestionModel(
-      id: json['id'] as String,
-      checklistId: json['checklistId'] as String? ?? '',
-      text: json['text'] as String,
-      type: json['type'] as String,
-      requiredField: json['requiredField'] as bool? ?? false,
-    );
-  }
+  factory ChecklistQuestionModel.fromMap(Map<String, dynamic> m) =>
+      ChecklistQuestionModel(
+        id: m['id'] as String,
+        checklistId: m['checklist_id'] as String,
+        text: m['text'] as String,
+        type: m['type'] as String,
+        requiredField: (m['required_field'] as int? ?? 1) == 1,
+      );
 }
