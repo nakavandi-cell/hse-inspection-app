1041797ccbcc8dda3bbef67ad2466738f8bbc8e9
nakavandi-cell <nakavandi@gmail.com>
2026-08-17 15:48:36 +0330

    Update file_saver_helper.dart

--- lib/utils/file_saver_helper.dart
+++ lib/utils/file_saver_helper.dart
@@ -1,15 +1,33 @@
-import 'dart:typed_data';
+import 'dart:io';
 
-import 'package:file_saver/file_saver.dart';
+import 'package:path_provider/path_provider.dart';
 
-Future<void> saveExcelBytes({
-  required List<int> bytes,
-  required String fileName,
-}) async {
-  await FileSaver.instance.saveFile(
-    name: fileName,
-    bytes: Uint8List.fromList(bytes),
-    ext: 'xlsx',
-    mimeType: MimeType.xlsx,
-  );
+class FileSaverHelper {
+  FileSaverHelper._();
+
+  static final FileSaverHelper instance = FileSaverHelper._();
+
+  Future<File> copyToDocuments(File sourceFile, {String? fileName}) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final targetName = fileName ?? sourceFile.path.split(Platform.pathSeparator).last;
+    final targetPath = '${directory.path}/$targetName';
+
+    final targetFile = File(targetPath);
+    return sourceFile.copy(targetFile.path);
+  }
+
+  Future<String> getDocumentsPath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
+  }
+
+  Future<File> saveBytesAsFile({
+    required List<int> bytes,
+    required String fileName,
+  }) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/$fileName');
+    await file.writeAsBytes(bytes, flush: true);
+    return file;
+  }
 }
