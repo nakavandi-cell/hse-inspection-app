ac494a4c51b6f17877f3cde1ddf2ce6fec1469a4
nakavandi-cell <nakavandi@gmail.com>
2026-08-17 15:16:23 +0330

    Update checklist_model.dart

--- lib/core/models/checklist_model.dart
+++ lib/core/models/checklist_model.dart
@@ -1,57 +1,53 @@
-import 'checklist_question_model.dart';
+// lib/models/checklist_model.dart
 
-class ChecklistModel {
+class Checklist {
   final String id;
   final String title;
   final String category;
-  final String? code;
-  final String? version;
-  final List<ChecklistQuestionModel> questions;
+  final String code;
+  final String version;
+  final List<ChecklistQuestion> questions;
 
-  const ChecklistModel({
+  Checklist({
     required this.id,
     required this.title,
     required this.category,
-    this.code,
-    this.version,
+    required this.code,
+    required this.version,
     required this.questions,
   });
 
-  factory ChecklistModel.fromJson(Map<String, dynamic> json) {
-    final rawQuestions = json['questions'] as List<dynamic>? ?? const [];
-    return ChecklistModel(
+  factory Checklist.fromJson(Map<String, dynamic> json) {
+    return Checklist(
       id: json['id'] as String,
       title: json['title'] as String,
       category: json['category'] as String,
-      code: json['code'] as String?,
-      version: json['version'] as String?,
-      questions: rawQuestions
-          .map((q) => ChecklistQuestionModel.fromJson(q as Map<String, dynamic>))
+      code: json['code'] as String,
+      version: json['version'] as String,
+      questions: (json['questions'] as List)
+          .map((q) => ChecklistQuestion.fromJson(q))
           .toList(),
     );
   }
 
-  Map<String, dynamic> toMap() => {
-        'id': id,
-        'title': title,
-        'category': category,
-        'code': code,
-        'version': version,
-      };
-
-  factory ChecklistModel.fromMap(
-    Map<String, dynamic> row,
-    List<Map<String, dynamic>> questionRows,
-  ) {
-    return ChecklistModel(
-      id: row['id'] as String,
-      title: row['title'] as String,
-      category: row['category'] as String,
-      code: row['code'] as String?,
-      version: row['version'] as String?,
-      questions: questionRows
-          .map((q) => ChecklistQuestionModel.fromMap(q))
-          .toList(),
-    );
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'category': category,
+      'code': code,
+      'version': version,
+      'questions': questions.map((q) => q.toJson()).toList(),
+    };
   }
 }
+
+class ChecklistQuestion {
+  final String id;
+  final String checklistId;
+  final String text;
+  final String type;
+  final bool requiredField;
+
+  ChecklistQuestion({
+    required this.id,
