77da21bee647992a1bbcf023b9c2a2a150493969
nakavandi-cell <nakavandi@gmail.com>
2026-08-17 12:28:58 +0330

    Create file_saver_helper.dart

---
+++ lib/utils/file_saver_helper.dart
@@ -0,0 +1,15 @@
+import 'dart:typed_data';
+
+import 'package:file_saver/file_saver.dart';
+
+Future<void> saveExcelBytes({
+  required List<int> bytes,
+  required String fileName,
+}) async {
+  await FileSaver.instance.saveFile(
+    name: fileName,
+    bytes: Uint8List.fromList(bytes),
+    ext: 'xlsx',
+    mimeType: MimeType.microsoftExcel,
+  );
+}
