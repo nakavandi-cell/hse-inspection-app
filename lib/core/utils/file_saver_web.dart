90af64029cda5b65931ad46a13d6243053781cf4
nakavandi-cell <nakavandi@gmail.com>
2026-08-28 11:47:04 +0330

    Update file_saver_web.dart

--- lib/core/utils/file_saver_web.dart
+++ lib/core/utils/file_saver_web.dart
@@ -1,14 +1,31 @@
-import 'dart:convert';
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'dart:io';
+import 'package:flutter/foundation.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
 
 class FileSaverHelper {
-  static void saveFileWeb(List<int> bytes, String fileName) {
-    final String base64 = base64Encode(bytes);
-    final String anchorAttributes = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,$base64';
-    
-    final html.AnchorElement anchor = html.AnchorElement(href: anchorAttributes)
-      ..setAttribute('download', fileName)
-      ..click();
+  static Future<void> saveFileWeb(List<int> bytes, String fileName) async {
+    try {
+      if (kIsWeb) {
+        // در محیط وب در صورت نیاز از پکیج‌های وب استفاده می‌شود
+        return;
+      }
+
+      // در محیط اندروید و iOS
+      Directory? directory;
+      if (Platform.isAndroid) {
+        directory = await getExternalStorageDirectory();
+      }
+      directory ??= await getApplicationDocumentsDirectory();
+
+      final String filePath = '${directory.path}/$fileName';
+      final File file = File(filePath);
+      await file.writeAsBytes(bytes, flush: true);
+
+      // باز کردن خودکار فایل اکسل در گوشی کاربر
+      await OpenFile.open(filePath);
+    } catch (e) {
+      debugPrint('Error saving/opening file: $e');
+    }
   }
 }
