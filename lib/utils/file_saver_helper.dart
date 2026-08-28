07dad729273d532be51b29c93fe527664ac9a898
nakavandi-cell <nakavandi@gmail.com>
2026-08-28 15:13:45 +0330

    Update file_saver_helper.dart

--- lib/utils/file_saver_helper.dart
+++ lib/utils/file_saver_helper.dart
@@ -1,33 +1,23 @@
 import 'dart:io';
-
 import 'package:path_provider/path_provider.dart';
 
 class FileSaverHelper {
-  FileSaverHelper._();
+  static Future<String> saveAndOpenFile(List<int> bytes, String fileName) async {
+    try {
+      Directory? directory;
+      if (Platform.isAndroid) {
+        directory = await getExternalStorageDirectory();
+      }
+      directory ??= await getApplicationDocumentsDirectory();
 
-  static final FileSaverHelper instance = FileSaverHelper._();
-
-  Future<File> copyToDocuments(File sourceFile, {String? fileName}) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final targetName = fileName ?? sourceFile.path.split(Platform.pathSeparator).last;
-    final targetPath = '${directory.path}/$targetName';
-
-    final targetFile = File(targetPath);
-    return sourceFile.copy(targetFile.path);
-  }
-
-  Future<String> getDocumentsPath() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return directory.path;
-  }
-
-  Future<File> saveBytesAsFile({
-    required List<int> bytes,
-    required String fileName,
-  }) async {
-    final directory = await getApplicationDocumentsDirectory();
-    final file = File('${directory.path}/$fileName');
-    await file.writeAsBytes(bytes, flush: true);
-    return file;
+      final file = File('${directory.path}/$fileName');
+      await file.writeAsBytes(bytes, flush: true);
+      return file.path;
+    } catch (e) {
+      final tempDir = await getTemporaryDirectory();
+      final file = File('${tempDir.path}/$fileName');
+      await file.writeAsBytes(bytes, flush: true);
+      return file.path;
+    }
   }
 }
