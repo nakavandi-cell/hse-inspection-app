7fc273c00bbcbcbabff8c7a882087d96e2284d20
nakavandi-cell <nakavandi@gmail.com>
2026-08-28 15:14:26 +0330

    Update excel_export_service.dart

--- lib/services/excel_export_service.dart
+++ lib/services/excel_export_service.dart
@@ -1,77 +1,45 @@
-import 'dart:typed_data';
 import 'package:excel/excel.dart';
 import '../core/models/inspection_model.dart';
-import '../core/utils/file_saver_web.dart';
+import '../utils/file_saver_helper.dart';
 
 class ExcelExportService {
-  static Future<void> exportInspection(InspectionModel inspection) async {
+  static Future<String> exportInspections(List<InspectionModel> inspections) async {
     final excel = Excel.createExcel();
-    final defaultSheet = excel.getDefaultSheet();
-    if (defaultSheet != null) {
-      excel.rename(defaultSheet, 'گزارش بازرسی');
-    }
-    final Sheet sheet = excel['گزارش بازرسی'];
+    final sheet = excel['گزارش بازرسی‌های HSE'];
+    excel.setDefaultSheet('گزارش بازرسی‌های HSE');
 
-    // Header styling
-    final cellStyleHeader = CellStyle(
-      bold: true,
-      horizontalAlign: HorizontalAlign.Center,
-      verticalAlign: VerticalAlign.Center,
-    );
+    // سرستون‌های فارسی
+    sheet.appendRow([
+      TextCellValue('شناسه بازرسی'),
+      TextCellValue('عنوان بازرسی'),
+      TextCellValue('کد مدرک'),
+      TextCellValue('نام چک‌لیست'),
+      TextCellValue('شناسه چک‌لیست'),
+      TextCellValue('تاریخ بازرسی'),
+      TextCellValue('وضعیت'),
+    ]);
 
-    // Meta Data Info
-    final dateStr = inspection.createdAt.toIso8601String().split('T').first;
-    final titleStr = inspection.checklistTitle ?? inspection.title ?? 'بازرسی';
-
-    sheet.appendRow([TextCellValue('عنوان بازرسی:'), TextCellValue(titleStr)]);
-    sheet.appendRow([TextCellValue('دسته‌بندی:'), TextCellValue(inspection.checklistCategory)]);
-    sheet.appendRow([TextCellValue('کد بازرسی:'), TextCellValue(inspection.checklistCode ?? inspection.id)]);
-    sheet.appendRow([TextCellValue('تاریخ بازرسی:'), TextCellValue(dateStr)]);
-    sheet.appendRow([TextCellValue('وضعیت:'), TextCellValue(inspection.status.name)]);
-    sheet.appendRow([TextCellValue('')]); // خالی
-
-    // Table Columns
-    final headers = [
-      TextCellValue('ردیف'),
-      TextCellValue('شناسه سوال'),
-      TextCellValue('وضعیت پاسخ'),
-      TextCellValue('توضیحات / اقدام اصلاحی'),
-      TextCellValue('زمان ثبت'),
-    ];
-    sheet.appendRow(headers);
-
-    for (int i = 0; i < headers.length; i++) {
-      final cell = sheet.cell(CellIndex.indexByColumnRow(columnIndex: i, rowIndex: 6));
-      cell.cellStyle = cellStyleHeader;
-    }
-
-    // Rows
-    if (inspection.answers.isEmpty) {
+    // درج سطرهای بازرسی
+    for (final item in inspections) {
       sheet.appendRow([
-        TextCellValue('1'),
-        TextCellValue('-'),
-        TextCellValue('پاسخی ثبت نشده است'),
-        TextCellValue('-'),
-        TextCellValue('-'),
+        TextCellValue(item.id?.toString() ?? '-'),
+        TextCellValue(item.title),
+        TextCellValue(item.checklistCode),
+        TextCellValue(item.checklistTitle),
+        TextCellValue(item.checklistId),
+        TextCellValue(item.date),
+        TextCellValue(item.status == 'completed' ? 'تکمیل شده' : 'در جریان'),
       ]);
-    } else {
-      for (int i = 0; i < inspection.answers.length; i++) {
-        final ans = inspection.answers[i];
-        sheet.appendRow([
-          TextCellValue((i + 1).toString()),
-          TextCellValue(ans.questionId),
-          TextCellValue(ans.status),
-          TextCellValue(ans.note),
-          TextCellValue(ans.answeredAt),
-        ]);
-      }
     }
 
     final bytes = excel.encode();
-    if (bytes != null) {
-      final safeName = titleStr.replaceAll(RegExp(r'[\\/:*?"<>| ]'), '_');
-      final fileName = 'HSE_Report_${safeName}_$dateStr.xlsx';
-      await FileSaverWeb.saveAndDownload(Uint8List.fromList(bytes), fileName);
+    if (bytes == null) {
+      throw Exception('خطا در تولید فایل اکسل');
     }
+
+    final timestamp = DateTime.now().millisecondsSinceEpoch;
+    final fileName = 'HSE_Inspections_Report_$timestamp.xlsx';
+
+    return await FileSaverHelper.saveAndOpenFile(bytes, fileName);
   }
 }
